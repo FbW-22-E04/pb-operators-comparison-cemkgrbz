de9e8c36c8f1739f92586e9c90e6aeebcfe7cb47
Cem <cemkgrbz@gmail.com>
2022-06-29 14:54:53 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,21 @@
+//0 - 1 - 2 - 3 - 4 - 5
+
+let apples = 20;
+let oranges = 30;
+console.log(apples == oranges);
+console.log(apples != oranges);
+console.log(apples > oranges);
+console.log(apples <= oranges);
+console.log(oranges > apples);
+
+//6
+
+console.log('--------------');
+let mangoes = 5;
+console.log(apples * mangoes);
+console.log(apples * mangoes > mangoes + oranges);
+console.log(apples - mangoes < oranges / mangoes);
+console.log(mangoes == apples == oranges);
+console.log(apples / mangoes == oranges / mangoes);
+console.log(apples + mangoes > oranges - mangoes);
+console.log(apples + mangoes == oranges - mangoes);
